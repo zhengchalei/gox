613a4a5dd6b47612d9185c12cd09b9d71a744bc8
zhengchalei <stone981023@gmail.com>
2025-05-17 22:47:12 +0800

    feat(auth): 优化自定义权限加载接口实现
    - 修改 StpInterfaceImpl 类，使用 KSqlClient 实现动态查询用户权限和角色
    - 更新 getPermissionList 和 getRoleList 方法，返回真实的权限和角色数据

--- gox-modules/gox-system/gox-auth/src/main/kotlin/com/zhengchalei/gox/modules/system/auth/config/StpInterfaceImpl.kt
+++ gox-modules/gox-system/gox-auth/src/main/kotlin/com/zhengchalei/gox/modules/system/auth/config/StpInterfaceImpl.kt
@@ -1,36 +1,35 @@
 package com.zhengchalei.gox.modules.system.auth.config
 
 import cn.dev33.satoken.stp.StpInterface
+import com.zhengchalei.gox.modules.system.entity.*
+import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.ast.expression.eq
 import org.springframework.stereotype.Component
 
 /**
  * 自定义权限加载接口实现类
  */
 @Component // 保证此类被 SpringBoot 扫描，完成 Sa-Token 的自定义权限验证扩展 
-class StpInterfaceImpl : StpInterface {
+class StpInterfaceImpl(
+    private val sqlClient: KSqlClient
+) : StpInterface {
     /**
      * 返回一个账号所拥有的权限码集合
      */
-    override fun getPermissionList(loginId: Any?, loginType: String?): MutableList<String?> {
-        // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询权限
-        val list: MutableList<String?> = ArrayList<String?>()
-        list.add("101")
-        list.add("user.add")
-        list.add("user.update")
-        list.add("user.get")
-        // list.add("user.delete");
-        list.add("art.*")
-        return list
+    override fun getPermissionList(loginId: Any, loginType: String?): List<String> {
+        return this.sqlClient.executeQuery(User::class) {
+            where(table.id eq loginId as Long)
+            select(table.asTableEx().roles.asTableEx().permissions.code)
+        }
     }
 
     /**
      * 返回一个账号所拥有的角色标识集合 (权限与角色可分开校验)
      */
-    override fun getRoleList(loginId: Any?, loginType: String?): MutableList<String?> {
-        // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询角色
-        val list: MutableList<String?> = ArrayList<String?>()
-        list.add("admin")
-        list.add("super-admin")
-        return list
+    override fun getRoleList(loginId: Any, loginType: String?): List<String> {
+        return this.sqlClient.executeQuery(User::class) {
+            where(table.id eq loginId as Long)
+            select(table.asTableEx().roles.code)
+        }
     }
 }
