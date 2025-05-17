af224054e9d875a48135b92905c1bf72757aab90
zhengchalei <stone981023@gmail.com>
2025-05-17 22:47:07 +0800

    feat(auth): 新增自定义权限加载接口实现类
    - 创建 StpInterfaceImpl 类，实现 Sa-Token 的自定义权限验证
    - 提供获取用户权限码和角色标识的功能

---
+++ gox-modules/gox-system/gox-auth/src/main/kotlin/com/zhengchalei/gox/modules/system/auth/config/StpInterfaceImpl.kt
@@ -0,0 +1,36 @@
+package com.zhengchalei.gox.modules.system.auth.config
+
+import cn.dev33.satoken.stp.StpInterface
+import org.springframework.stereotype.Component
+
+/**
+ * 自定义权限加载接口实现类
+ */
+@Component // 保证此类被 SpringBoot 扫描，完成 Sa-Token 的自定义权限验证扩展 
+class StpInterfaceImpl : StpInterface {
+    /**
+     * 返回一个账号所拥有的权限码集合
+     */
+    override fun getPermissionList(loginId: Any?, loginType: String?): MutableList<String?> {
+        // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询权限
+        val list: MutableList<String?> = ArrayList<String?>()
+        list.add("101")
+        list.add("user.add")
+        list.add("user.update")
+        list.add("user.get")
+        // list.add("user.delete");
+        list.add("art.*")
+        return list
+    }
+
+    /**
+     * 返回一个账号所拥有的角色标识集合 (权限与角色可分开校验)
+     */
+    override fun getRoleList(loginId: Any?, loginType: String?): MutableList<String?> {
+        // 本 list 仅做模拟，实际项目中要根据具体业务逻辑来查询角色
+        val list: MutableList<String?> = ArrayList<String?>()
+        list.add("admin")
+        list.add("super-admin")
+        return list
+    }
+}
