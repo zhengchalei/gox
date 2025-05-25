869f374fede2bf5dcb8907d3a9598751454e811a
zhengchalei <stone981023@gmail.com>
2025-05-26 02:40:53 +0800

    refactor(config): 重命名配置类为 JimmerDBConfig
    - 将 Config.kt 文件重命名为 JimmerDBConfig.kt
    - 更新类名以更好地反映其功能和用途
    - 保留原有的功能代码不变

--- gox-framework/src/main/kotlin/com/zhengchalei/gox/framework/config/JimmerDBConfig.kt
+++ gox-framework/src/main/kotlin/com/zhengchalei/gox/framework/config/JimmerDBConfig.kt
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.stereotype.Component
 
 @Component
-class Config : BeanFactoryPostProcessor {
+class JimmerDBConfig : BeanFactoryPostProcessor {
     @Throws(BeansException::class)
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+        // 强制让 sqlClient Bean 的初始化 依赖于另一个名称完全等于 "liquibase" 的 Bean
         val jimmeBeanDefinition = beanFactory.getBeanDefinition("sqlClient")
         jimmeBeanDefinition.setDependsOn("liquibase")
     }
